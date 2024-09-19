ca28843bd5b9f4ab1bc830f6b017361127be4153
AgentK <dmitryk@3vium.com>
2024-09-19 20:45:48 +0300

    lesson 4 k-NN py

---
+++ l4.k-NN.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# flake8: noqa
+# noqa: E501
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+# Загрузка датасета Iris
+iris = load_iris()
+X, y = iris.data, iris.target
+
+# Разделение данных на обучающую и тестовую выборки
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Создание и обучение модели k-NN (k=3)
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X_train, y_train)
+
+# Прогнозирование на тестовой выборке
+y_pred_knn = knn.predict(X_test)
+
+
+# Оценка модели
+print("k-NN Accuracy:", accuracy_score(y_test, y_pred_knn))
+print("Classification Report:\n", classification_report(y_test, y_pred_knn))
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred_knn))
