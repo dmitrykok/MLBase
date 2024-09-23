5d12f21b39c8473a3239632b79317b4c67174557
AgentK <dmitryk@3vium.com>
2024-09-23 18:17:48 +0300

    update

--- code/l4.DT.py
+++ code/l4.DT.py
@@ -5,17 +5,19 @@
 
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 # Загрузка датасета Iris
 iris = load_iris()
 X, y = iris.data, iris.target
 
 # Разделение данных на обучающую и тестовую выборки
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 # Создание и обучение модели дерева решений
 dt = DecisionTreeClassifier(max_depth=3)
