f9a7e0e300dec78e57d9f7b7cf0bf5e5ab0cb24f
AgentK <dmitryk@3vium.com>
2024-10-13 15:17:53 +0300

    cosmetics

--- code/l4.k-NN.py
+++ code/l4.k-NN.py
@@ -13,7 +13,9 @@ iris = load_iris()
 X, y = iris.data, iris.target
 
 # Разделение данных на обучающую и тестовую выборки
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 # Создание и обучение модели k-NN (k=3)
 knn = KNeighborsClassifier(n_neighbors=3)
